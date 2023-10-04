gradient with bug

Net/MyNet.py
@@ -1,5 +1,8 @@
 import numpy as np
 from Util.function import sigmoid, softmax, cross_entropy_error, numerical_gradient, mean_squared_error, relu
+from Layer.Affine import Affine
+from Layer.Sigmoid import Sigmoid
+from collections import OrderedDict
 
 
 class MyNet:
@@ -12,18 +15,19 @@ class MyNet:
         self.params['W2'] = weight_init_std * \
             np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
-        
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(
+            self.params['W1'], self.params['b1'])
+        self.layers['Sigmoid1'] = Sigmoid()
+        self.layers['Affine2'] = Affine(
+            self.params['W2'], self.params['b2'])
 
     def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        z2 = a2
-
-        return z2
+        return x
 
     def loss(self, x, t):
         y = self.predict(x)
@@ -46,5 +50,25 @@ class MyNet:
         grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
         grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    def gradient(self, x, t):
+        # forward
+        self.predict(x)
+
+        # backward
+        dout = 1
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 设置
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dw
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dw
+        grads['b2'] = self.layers['Affine2'].db
 
         return grads

main.py
@@ -4,15 +4,12 @@ from Util.function import mean_squared_error, sigmoid, softmax, cross_entropy_er
 from Net.MyNet import MyNet
 import matplotlib.pyplot as plt
 
-iter_num = 100000
+iter_num = 10000
 train_size = 100
 learning_rate = 0.001
 test_size = 100
 X = np.linspace(-np.pi, np.pi, train_size).reshape(1, train_size).T
 t = np.sin(X)
-plt.scatter(X, t, c='y', linewidths=0.1, marker='o')
-plt.title('test: +, train: o')
-plt.show()
 
 mynet = MyNet(1, 10, 1)
 
@@ -20,8 +17,9 @@ mynet = MyNet(1, 10, 1)
 for i in range(iter_num):
     z = mynet.predict(X)
     loss = mean_squared_error(z, t)
-    grads = mynet.numerical_gradient(X, t)
+    grads = mynet.gradient(X, t)
     for key in ("W1", "b1", "W2", "b2"):
+        # print(key, grads[key])
         mynet.params[key] -= learning_rate * grads[key]
     if (i % 100 == 0):
         print(i, loss)